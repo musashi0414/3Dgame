960c2cad6bd0b1ca4702d80ae541f0851698f76a
DESKTOP-M30FNO0\GA2A <h1202319006@hiratagakuen.onmicrosoft.com>
2024-10-18 16:08:05 +0900

    照準を表示

---
+++ Assets/Script/Destroy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destroy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Õ“Ë‚µ‚½‘Šè‚ÉPlayerƒ^ƒO‚ª•t‚¢‚Ä‚¢‚é‚Æ‚«
+        if (collision.gameObject.tag == "gun")
+        {
+            // 0.2•bŒã‚ÉÁ‚¦‚é
+            Destroy(this.gameObject, 0.2f);
+        }
+    }
+}
