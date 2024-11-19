5d52387a0d7d3dd00e0739e812fbee72b5a184a8
DESKTOP-M30FNO0\GA2A <h1202319006@hiratagakuen.onmicrosoft.com>
2024-11-19 16:32:53 +0900

    no message

--- Assets/Script/ScoreManager.cs
+++ Assets/Script/ScoreManager.cs
@@ -5,19 +5,19 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    private Text scoreText;
+    private Text ScoreCount;
     public int score = 0;
 
     // Start is called before the first frame update
     void Start()
     {
-        scoreText = GetComponentInChildren<Text>();
-        scoreText.text = "0";
+        ScoreCount = GetComponentInChildren<Text>();
+        ScoreCount.text = "0";
     }
 
     // Update is called once per frame
     void Update()
     {
-        scoreText.text = score.ToString();
+        ScoreCount.text = score.ToString();
     }
 }
