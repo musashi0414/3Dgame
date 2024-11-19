2daffc7526a796d13e70852009bc9e5755a7f7e3
DESKTOP-M30FNO0\GA2A <h1202319006@hiratagakuen.onmicrosoft.com>
2024-11-19 16:32:39 +0900

    スコアUI作成

--- Assets/Script/CubeMoveUp.cs
+++ Assets/Script/CubeMoveUp.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class CubeMoveUp : MonoBehaviour
 {
+    private int count = 0;
+    private GameObject scoreText; // ç°âÒí«â¡â”èä
+
     Vector3 startPos;
     // Start is called before the first frame update
     void Start()
     {
         startPos = transform.position;
+
+        ScoreCount = GameObject.Find("ScoreCount"); // ç°âÒí«â¡â”èä 
     }
 
     // Update is called once per frame
@@ -16,5 +21,15 @@ public class CubeMoveUp : MonoBehaviour
     {
         float posY = startPos.y + Mathf.Sin(Time.time) * 4;
         transform.position = new Vector3(transform.position.x, posY, transform.position.z);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "gun")
+        {
+            Debug.Log("BulletCollision");
+            ScoreCount.GetComponent<ScoreManager>().score = ScoreCount.GetComponent<ScoreManager>().score + 3; // ç°âÒí«â¡â”èä
+            Destroy(this.gameObject);
+        }
     }
 }
