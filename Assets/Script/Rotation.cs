539b2eaceadb8a23db82e5cfbb1b5fd85920b712
DESKTOP-M30FNO0\GA2A <h1202319006@hiratagakuen.onmicrosoft.com>
2025-01-21 16:25:19 +0900

    音調整

--- Assets/Script/Rotation.cs
+++ Assets/Script/Rotation.cs
@@ -11,15 +11,26 @@ public class Rotation : MonoBehaviour
     //private int highScore;// ハイスコア
 
     //public int value = 0;
+    // AudioSourceを格納する変数
+    private AudioSource audioSource;
 
+    // 効果音のAudioClipを設定する変数
+    public AudioClip soundEffect;
 
     // Start is called before the first frame update
     void Start()
     {
+        // AudioSourceコンポーネントを取得
+        audioSource = GetComponent<AudioSource>();
 
+        // AudioClipが設定されていれば、AudioSourceに適用
+        if (soundEffect != null)
+        {
+            audioSource.clip = soundEffect;
+        }
     }
 
-    void OnTriggerEnter(Collider other)
+    private void OnCollisionEnter(Collision collision)
     {
         //if (other.gameObject.CompareTag("Blue"))
         //{
@@ -39,8 +50,16 @@ public class Rotation : MonoBehaviour
 
         //score += 3;
         //ScoreText.text = string.Format("スコア:{0}", score);
+        //if (other.gameObject.tag == "gun") // 青色かどうかのタグをチェック
+        //{
+        //    GetComponent<AudioSource>().Play();  // 効果音を鳴らす
 
-        GetComponent<AudioSource>().Play();  // 効果音を鳴らす
+        //}
+        // 効果音を再生
+        if (audioSource != null && !audioSource.isPlaying)
+        {
+            audioSource.PlayOneShot(soundEffect);
+        }
     }
 
     // Update is called once per frame
